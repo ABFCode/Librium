295162ca201bad3729ba1b7e38243106a66e061c
Aidan Finnegan <71090977+ABFCode@users.noreply.github.com>
2025-03-06 18:29:06 -0500

    trying to remake dev env and not break things

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const isDev =  === "development";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['librium.dev']
-  }
-})
+    allowedHosts: ["librium.dev"],
+  },
+});
