de31a483baf7728e769f509361a1e8e3c897b839
Aidan Finnegan <71090977+ABFCode@users.noreply.github.com>
2025-04-04 18:55:02 -0400

    more work on delete, default book WIP

--- SpringReader/src/main/java/com/example/springreader/service/BookInitService.java
+++ SpringReader/src/main/java/com/example/springreader/service/BookInitService.java
@@ -1,23 +1,54 @@
 package com.example.springreader.service;
 
+import com.example.springreader.model.Book;
 import com.example.springreader.repository.BookRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 /**
  * Service responsible for initializing the app with some default books.
  * Only populates if DB is empty on startup.
  */
-@Service
 @Slf4j
+@Service
+@RequiredArgsConstructor
 public class BookInitService {
     private final BookRepository bookRepository;
-    //change to @value
-    private final String BOOKS_PATH = "src/main/resources/files/defaults";
+    private final ResourceLoader resourceLoader;
 
 
-    public BookInitService(BookRepository bookRepository){
-        this.bookRepository = bookRepository;
+    @Value("${default.book.path}")
+    private String defaultBookPath;
+
+
+
+    @PostConstruct
+    @Transactional
+    public void initializeDefaultBook(){
+        Optional<Book> existingDefaultBook = bookRepository.findByisDefaultTrue();
+
+        if(existingDefaultBook.isEmpty()){
+            try {
+                Resource resource = resourceLoader.getResource(defaultBookPath);
+                if(!resource.exists()){
+                    log.error("Default book file not found");
+                }
+                else{
+                    log.info("Default book file found");
+                }
+            } catch (Exception e) {
+                log.error("Error loading default book file");
+            }
+        }
+
     }
 
 
