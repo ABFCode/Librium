5db7526c93fa95913fd1009f1bd0a7a57b930db2
Aidan Finnegan <71090977+ABFCode@users.noreply.github.com>
2025-04-04 21:50:58 -0400

    small doc updapte

--- Frontend/src/components/Library/Library.tsx
+++ Frontend/src/components/Library/Library.tsx
@@ -187,7 +187,6 @@ function Library() {
                   viewBox="0 0 16 16"
                   xmlns="http://www.w3.org/2000/svg"
                   stroke="currentcolor"
-                  fill="#000000"
                 >
                   <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
                 </svg>
