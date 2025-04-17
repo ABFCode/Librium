09775c203077052e5ad8ea1863f8f3a1e31c8876
Aidan Finnegan <71090977+ABFCode@users.noreply.github.com>
2025-04-16 20:28:18 -0400

    fix for mobile nav

--- SpringReader/src/main/java/com/example/springreader/controller/LibraryController.java
+++ SpringReader/src/main/java/com/example/springreader/controller/LibraryController.java
@@ -7,7 +7,6 @@ import com.example.springreader.model.UserBook;
 import com.example.springreader.service.LibraryService;
 import com.example.springreader.service.UserBookService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -19,7 +18,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -89,16 +87,15 @@ public class LibraryController {
 
     }
 
-    @GetMapping()
-    public ResponseEntity<Resource> downloadBook(@PathVariable Long bookId, @AuthenticationPrincipal User user) throws IOException {
-
-        Map<String, Object> bookInfo = libraryService.getBookResources(bookId, user.getId());
-        String filename = (String) bookInfo.get("filename");
-        Resource bookData = (Resource) bookInfo.get("bookData");
-
-
-        return null;
-    }
+//    @GetMapping()
+//    public ResponseEntity<Resource> downloadBook(@PathVariable Long bookId, @AuthenticationPrincipal User user) throws IOException {
+//
+//        Map<String, Object> bookInfo = libraryService.getBookResources(bookId, user.getId());
+//        String filename = (String) bookInfo.get("filename");
+//        Resource bookData = (Resource) bookInfo.get("bookData");
+//
+//        return null;
+//    }
 
     @GetMapping()
     public ResponseEntity<List<BookDTO>> getUserBooks(@AuthenticationPrincipal User user){
