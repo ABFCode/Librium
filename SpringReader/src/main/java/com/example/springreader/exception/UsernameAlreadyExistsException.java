612462a929c0dfe247f3d4c7a8ac3720114234b9
Aidan Finnegan <71090977+ABFCode@users.noreply.github.com>
2025-04-24 20:19:20 -0400

    usernames to lowercase to prevent case sensitive users

--- SpringReader/src/main/java/com/example/springreader/exception/UsernameAlreadyExistsException.java
+++ SpringReader/src/main/java/com/example/springreader/exception/UsernameAlreadyExistsException.java
@@ -6,7 +6,7 @@ package com.example.springreader.exception;
  */
 public class UsernameAlreadyExistsException extends RuntimeException{
     public UsernameAlreadyExistsException(String username) {
-        super("Username " + username + " already exists");
+        super("Username " + username.toLowerCase() + " already exists");
     }
 
 }
